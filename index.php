fcb4a00a578ebf41b090902f268885f8b518676d
Hebert Porto <hebertporto@gmail.com>
2014-08-11 16:13:33 -0300

    Ajustes no index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Balanced Team Company</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+        <link rel="icon"  type="image/png" href="/images/favicon.ico">
+    </head>
+    <body>
+        <div class="container">
+            <?php require_once('incs/main_menu.php'); ?>
+            <?php require_once('incs/home.php'); ?>
+        </div>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    </body>
+</html>
